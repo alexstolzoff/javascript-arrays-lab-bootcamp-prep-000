f284473bfb583511f5668d2bb7895cd6624bb8f8
Alex Stolzoff <alexstolznut@hotmail.com>
2017-08-17 23:10:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function destructivelyRemoveFirstKitten(){
 
 function appendKitten(name){
   const newKittens = [...kittens, name];
-  return kittens;
+  return newKittens;
 }
 
 // function prependKittens(name){
