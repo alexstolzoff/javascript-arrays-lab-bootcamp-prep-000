73b26978036b5aee2c0e9db08f0a0e84c3afe24e
Alex Stolzoff <alexstolznut@hotmail.com>
2017-08-17 22:58:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,38 +7,38 @@ function destructivelyAppendKitten(name){
   return kittens;
 }
 
-function destruvtivelyPrependKitten(name){
-  kittens.unshift(name);
-  return kittens;
-}
-
-function destructivelyRemoveLastKitten(){
-  kittens.pop();
-  return kittens;
-}
-
-function destructivelyRemoveFirstKitten(){
-  kittens.shift();
-  return kittens;
-}
-
-function appendKitten(name){
-  kittens = [...kittens, name];
-  return kittens;
-}
-
-function prependKittens(name){
-  kittens = [name, ...kittens];
-  return kittens;
-}
-function removeLastKitten(){
-  kittens = slice(0, kittens.length -1);
-  return kittens;
-}
-function removeFirstKitten(){
-  kittens = kittens.slice(1);
-  return kittens;
-}
+// function destruvtivelyPrependKitten(name){
+//   kittens.unshift(name);
+//   return kittens;
+// }
+// 
+// function destructivelyRemoveLastKitten(){
+//   kittens.pop();
+//   return kittens;
+// }
+// 
+// function destructivelyRemoveFirstKitten(){
+//   kittens.shift();
+//   return kittens;
+// }
+// 
+// function appendKitten(name){
+//   kittens = [...kittens, name];
+//   return kittens;
+// }
+// 
+// function prependKittens(name){
+//   kittens = [name, ...kittens];
+//   return kittens;
+// }
+// function removeLastKitten(){
+//   kittens = slice(0, kittens.length -1);
+//   return kittens;
+// }
+// function removeFirstKitten(){
+//   kittens = kittens.slice(1);
+//   return kittens;
+// }
 destructivelyAppendKitten("Ralph");
 // destructivelyPrependKitten("socks");
 // destructivelyRemoveLastKitten();
